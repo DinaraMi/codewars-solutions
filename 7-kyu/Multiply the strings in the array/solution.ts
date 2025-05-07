43ed0bfb7ac24f762a6757c746ece0e2d7f179bd
Dinara Mikhailova <d.mihaylova@keep-calm.ru>
2025-05-07 11:45:01 +0200

    добавила описание к задачам

--- 7-kyu/Multiply the strings in the array/solution.ts
+++ 7-kyu/Multiply the strings in the array/solution.ts
@@ -6,3 +6,13 @@ function arrMultiply(arr:[]) {
     const result = num1 * num2;
     return result.toString()
 }
+
+// DESCRIPTION:
+
+// You received an array with two strings.
+// Create a function that will return their product as a string.
+//     E.g.
+
+//     input: ['3', '5'] => output: '15'
+// input: ['2', '-3'] => output: '-6'
+// input: ['9', '0'] => output: '0'
--- 7-kyu/Scrolling Text/solution.ts
+++ 7-kyu/Scrolling Text/solution.ts
@@ -2,3 +2,22 @@ function scrollingText(text: string) {
     text = text.toUpperCase();
     return [...text].map((_, i) => text.slice(i) + text.slice(0, i));
 }
+
+// DESCRIPTION:
+
+// Let's create some scrolling text!
+
+// Your task is to complete the function which takes a string, and returns an array with all possible rotations of the given string, in uppercase.
+
+//     Example
+
+// scrollingText("codewars") should return:
+
+// ["CODEWARS",
+//     "ODEWARSC",
+//     "DEWARSCO",
+//     "EWARSCOD",
+//     "WARSCODE",
+//     "ARSCODEW"
+//   "RSCODEWA",
+//     "SCODEWAR"]
